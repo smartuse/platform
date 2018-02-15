d317bc32066fec06281e38e1d7326a6bec42a984
Oleg Lavrovsky <oleg@datalets.ch>
2018-02-15 13:31:41 +0100

    (WIP) datapackage loader

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,12 +1,16 @@
-
 /*!
- * Module dependencies.
+ * Home views
  */
+const Package = require('datapackage');
 
 exports.index = function (req, res) {
   res.render('index');
 };
 
 exports.sample = function (req, res) {
+  const dataPackage = Package.load('projects/sample_project/datapackage.json');
+  const resource = dataPackage.getResource('office-locations-geojson');
+  resource.read().then(function(data) { console.log(data); console.log('read'); });
+  console.log(resource);
   res.render('projects/sample_project/project');
 };
