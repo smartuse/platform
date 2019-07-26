5d58f2e105c8344a2e04ff9d673af800744c6cdd
Oleg Lavrovsky <oleg@utou.ch>
2019-07-26 17:25:38 +0200

    Refactoring (WIP)

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn backend.app:app -b 0.0.0.0:$PORT -w 3 --log-file=-
+web: gunicorn app:app -b 0.0.0.0:$PORT -w 3 --log-file=-
 init: flask db upgrade
--- platform/app.py
+++ platform/app.py
@@ -45,7 +45,9 @@ app.debug = True
 app.config.from_pyfile('config.py')
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
 
-# TODO: in the future, consider moving to Rest+ https://flask-restplus.readthedocs.io/
+# TODO: move app to Blueprints
+
+# TODO: move to Rest+ https://flask-restplus.readthedocs.io/
 #    version='1.0', title='Smart Use API',
 #    description='An API based on Frictionless Data standards')
 
@@ -58,7 +60,7 @@ with open(ospath.join(ospath.dirname(__file__), 'templates','presets','project-c
     project_categories = json.load(f)
 screenshot_path = ospath.join(ospath.dirname(__file__), '..', 'screenshots')
 upload_path = ospath.join(ospath.dirname(__file__), '..', 'uploads')
-DEFAULT_THUMB = '../img/usermap.jpg'
+DEFAULT_THUMB = './img/usermap.jpg'
 
 # Create admin
 admin = admin.Admin(app, name='SmartUse', template_mode='bootstrap3')
@@ -462,40 +464,40 @@ def project_page(project):
 # Static paths
 @app.route('/img/<path:path>')
 def send_static_img(path):
-    return send_from_directory('../static/img', path)
+    return send_from_directory('./static/img', path)
 @app.route('/scripts/<path:path>')
 def send_static_scripts(path):
-    return send_from_directory('../static/scripts', path)
+    return send_from_directory('./static/scripts', path)
 @app.route('/styles/<path:path>')
 def send_static_styles(path):
-    return send_from_directory('../static/styles', path)
+    return send_from_directory('./static/styles', path)
 @app.route('/tags/<path:path>')
 def send_static_tags(path):
-    return send_from_directory('../static/tags', path)
+    return send_from_directory('./static/tags', path)
 @app.route('/vendor/<path:path>')
 def send_static_vendor(path):
-    return send_from_directory('../static/vendor', path)
+    return send_from_directory('./static/vendor', path)
 @app.route('/meta/<path:path>')
 def send_static_meta(path):
-    return send_from_directory('../static/meta', path)
+    return send_from_directory('./static/meta', path)
 
 @app.route('/theme/<path:path>')
 def send_static_theme(path):
-    return send_from_directory('../static/theme', path)
+    return send_from_directory('./static/theme', path)
 @app.route('/screenshots/<path:path>')
 def send_screenshots(path):
-    return send_from_directory('../screenshots', path)
+    return send_from_directory('./screenshots', path)
 
 # Data serving paths
 @app.route('/data/<path:path>')
 def send_static_data(path):
-    return send_from_directory('../views/projects', path)
+    return send_from_directory('./views/projects', path)
 @app.route('/uploads/<path:path>')
 def send_uploads(path):
-    return send_from_directory('../uploads', path)
+    return send_from_directory('./uploads', path)
 @app.route('/api/project/<int:pid>/<path:path>')
 def send_uploads_project(pid, path):
-    return send_from_directory('../uploads', path)
+    return send_from_directory('./uploads', path)
 
 # Error paths
 @app.errorhandler(404)
